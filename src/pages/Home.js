a78d7285ac714d0c892570ab1011d7edb7083f5a
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-10-20 02:04:00 +0100

    Add function for searching by country

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,40 @@
+/* eslint-disable max-len */
 import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import SummaryCard from '../components/SummaryCard';
+import CountryCard from '../components/CountryCard';
 
 const Home = () => {
   const summary = useSelector((state) => state.summary);
   const countries = useSelector((state) => state.countries);
+  const [state, setState] = useState(countries);
+
+  const handleSearch = (e) => {
+    let filtered = [...countries].filter((country) => country.country.toLowerCase().includes(e.target.value.toLowerCase()));
+    filtered = !filtered.length ? [] : filtered;
+    setState(() => filtered);
+  };
+
+  useEffect(() => {
+    setState(() => countries);
+  }, [countries]);
 
   return (
-    <ul>
-      <li>{JSON.stringify(summary)}</li>
-      <li>{JSON.stringify(countries)}</li>
-    </ul>
+    <main>
+      <SummaryCard summary={summary} />
+      <section className="filter">
+        <input placeholder="Search Country" onChange={handleSearch} />
+      </section>
+      <section className="card-country">
+        <ul className="card-country__ul">
+          {state
+            && state.map((country) => (
+              // eslint-disable-next-line no-underscore-dangle
+              <CountryCard key={country.country} country={country} />
+            ))}
+        </ul>
+      </section>
+    </main>
   );
 };
 
